7049f8a7e81589a0dd92216fbf7484f8e0945dfe
senthilbalajiganesan <g.senthilbalaji@gmail.com>
2017-12-25 15:01:01 +0530

    Divided into folders

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,2 @@
-export { default as colors } from "./colors";
 export { default as constants } from "./constants";
 export * from "./helper";
-export * from "./ApiCalls";
-export * from "./ApiURLs";
